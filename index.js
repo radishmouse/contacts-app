db2634e152ebf42389dfa4b5c3c1dfc38bcee513
Chris Aquino <chris.aquino@gmail.com>
2018-06-18 14:19:20 -0400

    adds a second layout file for the homepage

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.set('view engine', '.hbs');
 app.get('/', (req, res) => {
   // res.send('HAAAAAAAAAYyyy');
   res.render('home', {
+    layout: 'homepage',
     message: "Welcome to the contacts app!",
     headerText: "Contacts App Home Page"
   });

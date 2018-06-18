a1dc489637e42aac58d04fc30063f24e831c3339
Chris Aquino <chris.aquino@gmail.com>
2018-06-18 12:26:18 -0400

    shows list of contacts in hbs template

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ app.get('/', (req, res) => {
 
 // Contacts List page: show the user all contacts
 app.get('/contacts', (req, res) => {
-  res.send(contacts.users);
+  // res.send(contacts.users);
+  res.render('contacts-list', {
+    contactsArray: contacts.users
+  });
 });
 
 // Contacts Detail page: show the user all info for one contact
